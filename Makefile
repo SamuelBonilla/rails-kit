d5663de74e9bd411bd940c08863f0ccccb362780
Sam Bonill <pythonners@gmail.com>
2022-12-28 22:32:27 -0500

    add devise

--- Makefile
+++ Makefile
@@ -8,4 +8,7 @@ migrate:
 rdbg:
 	bundle exec rdbg --open -n -c -- bundle exec rails s
 
+reset-db:
+	rails db:drop db:create db:migrate
+
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :auth, controllers: {
+    registrations: 'auth/registrations'
+  }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
