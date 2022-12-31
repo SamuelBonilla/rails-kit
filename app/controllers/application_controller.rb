7c3a944d457e2b4c376347a30ef6f0bfea057c09
Sam Bonill <pythonners@gmail.com>
2022-12-31 00:09:57 -0500

    an importan comment :)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,9 @@ class ApplicationController < ActionController::Base
   helper_method :has_role?
 
   def has_role?(role_name)
+    # we can improve this behavior by adding this function to the user class
+    # and when the user is login set the current user role and then validate
+    # this data to avoid consulting the database in each call to this function
     if auth_signed_in?
       current_auth.auth_membership.all.each do |role|
         is_role = AuthGroup.find_by(id: role.auth_group_id, role: role_name)
