6b839e7e5f6f349f699bb862c5a464cb502cfcd2
Sam Bonill <pythonners@gmail.com>
2022-12-31 09:50:53 -0500

    update git ignore

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   mount Motor::Admin => '/motor_admin'
-  devise_for :auth, controllers: {
-    registrations: 'auth/registrations'
-  }
+  devise_for :auth
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
